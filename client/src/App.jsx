0ce58f83c8f0f556fe46c537240a870c8c194d42
saitejadattu <bodasaiteja@gmail.com>
2025-04-23 18:16:22 +0530

    added login functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { BrowserRouter as Routers, Router, Route } from "react-router-dom";
+import { BrowserRouter as Routers, Route,Routes } from "react-router-dom";
 const App = () => {
   return (
     <Routers>
-      <Router>
+      <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
@@ -16,7 +16,7 @@ const App = () => {
             </div>
           }
         />
-      </Router>
+      </Routes>
     </Routers>
   );
 };
